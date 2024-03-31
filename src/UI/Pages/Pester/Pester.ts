57870dce674832a5f9aa103c841f8cd282a8697b
Luke Bates <lnbates20@gmail.com>
2024-03-30 22:39:45 -0400

    Added submit button for the pester popup, fixed pester CSS

--- src/UI/Pages/Pester/Pester.ts
+++ src/UI/Pages/Pester/Pester.ts
@@ -5,16 +5,20 @@ import CSS from "./Pester.module.css"
 
 class PesterPage extends Page {
     
-    inputField: HTMLInputElement
-    backButton: HTMLButtonElement
+    inputField  : HTMLInputElement
+    enterButton : HTMLButtonElement
+    backButton  : HTMLButtonElement
     
     constructor() {
         super()
         this.element.classList.add(CSS.Container)
-        this.inputField = Dom.Input(CSS.InputBox)
-        this.backButton = Dom.Button('Back')
+        
+        this.inputField  = Dom.Input(CSS.InputBox)
+        this.backButton  = Dom.Button('Back', CSS.Button)
+        this.enterButton = Dom.Button('Submit', CSS.Button)
         
         this.element.appendChild(this.inputField)
+        this.element.appendChild(this.enterButton)
         this.element.appendChild(this.backButton)
         
         this.eventman.AddEvent(this.backButton, 'click', () => this.HidePesterPrompt())
